82d7556da5930fd9a63d5d57c3735c49f1540357
Mizanur Rahman <engrmizu@gmail.com>
2020-05-16 12:12:43 +0600

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,24 @@
+ 
+<script type="text/javascript" src="<?=base_url();?>assets/js/moment.min.js"></script>
+<!--<script type="text/javascript" src="--><?//=base_url();?><!--assets/js/umd/pooper.min.js"></script>-->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
+<script type="text/javascript" src="<?=base_url();?>assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="<?=base_url();?>assets/js/dataTables.bootstrap.min.js"></script>
+<script type="text/javascript" src="<?=base_url();?>assets/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="<?=base_url();?>assets/js/bootstrap-datetimepicker.min.js"></script>
+<script type="text/javascript" src="<?=base_url();?>assets/js/bootstrap-datepicker.min.js"></script>
+<script type="text/javascript" src="<?=base_url();?>assets/js/bootstrap-select.min.js"></script>
+<script type="text/javascript" src="<?=base_url();?>assets/js/Chart.bundle.min.js"></script>
+<script type="text/javascript" src="<?=base_url();?>assets/js/Chart.min.js"></script>
+<script type="text/javascript" src="<?=base_url();?>assets/js/jquery.slimscroll.js"></script>
+<script type="text/javascript" src="<?=base_url();?>js/jquery-ui.js"></script>
+<!--<script type="text/javascript" src="--><?//=base_url();?><!--assets/js/select2.min.js"></script>-->
+<script type="text/javascript" src="<?=base_url();?>assets/js/app.js"></script>
+<script>
+    $(document).ready( function () {
+        $('.datatable').DataTable();
+    } );
+</script>
+</body>
+
+</html>
