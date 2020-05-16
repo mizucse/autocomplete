81b155126c6ad5e7794f9768aa0d32b707e871db
Mizanur Rahman <engrmizu@gmail.com>
2020-05-16 12:36:28 +0600

    Create custom.js

---
+++ custom.js
@@ -0,0 +1,33 @@
+function links()
+{	
+		var li="https://www.website.com/folder/";
+		//var li="http://localhost/demo/";
+		return li;
+}
+$(document).ready(function () {
+    li = links();
+    vv = 0;
+});
+
+
+$("#tag")
+    .keyup(function () {
+        var v = $("#tag").val();
+        if (v != '') {
+            $('#tag').addClass('ac_loading');
+            $("#tag").autocomplete({
+                source: function (request, response) {
+                    $.ajax({
+                        type: 'POST',
+                        dataType: 'json',
+                        url: li + 'transaction/autocomplete_view/',
+                        data: {id: v},
+                        success: function (data) {
+                            response(data);
+                        }
+                    });
+                    $("#tag").removeClass('ac_loading');
+                }
+            });
+        }
+    });
